471bac35a2757b3a3fd2d85ab8ea1c25340eeeb9
Eroc <eroccatlun@gmail.com>
2015-03-17 21:14:17 -0700

    Initial Commit for GenericImageBatcher.
    This commit brings a basic .exe which will convert a specific directory structure into the desired outputs.

---
+++ BatchConverter/BatchConverter/MainWindow.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace BatchConverter
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Extensions_Echo = new System.Windows.Forms.TextBox();
+            this.Extensions = new System.Windows.Forms.TextBox();
+            this.Type_Echo = new System.Windows.Forms.TextBox();
+            this._Type = new System.Windows.Forms.TextBox();
+            this.Res_Echo = new System.Windows.Forms.TextBox();
+            this._Res = new System.Windows.Forms.TextBox();
+            this.Material_Echo = new System.Windows.Forms.TextBox();
+            this._Material = new System.Windows.Forms.TextBox();
+            this.Catagory_Echo = new System.Windows.Forms.TextBox();
+            this._Catagory = new System.Windows.Forms.TextBox();
+            this.text_ActiveDirectory = new System.Windows.Forms.TextBox();
+            this.button_ChooseActiveDirectory = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(517, 558);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tableLayoutPanel2);
+            this.panel2.Controls.Add(this.text_ActiveDirectory);
+            this.panel2.Controls.Add(this.button_ChooseActiveDirectory);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(511, 273);
+            this.panel2.TabIndex = 5;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.Extensions_Echo, 1, 4);
+            this.tableLayoutPanel2.Controls.Add(this.Extensions, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.Type_Echo, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this._Type, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.Res_Echo, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this._Res, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.Material_Echo, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this._Material, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.Catagory_Echo, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this._Catagory, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(75, 28);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(436, 245);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // Extensions_Echo
+            // 
+            this.Extensions_Echo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Extensions_Echo.Location = new System.Drawing.Point(221, 199);
+            this.Extensions_Echo.Multiline = true;
+            this.Extensions_Echo.Name = "Extensions_Echo";
+            this.Extensions_Echo.ReadOnly = true;
+            this.Extensions_Echo.Size = new System.Drawing.Size(212, 43);
+            this.Extensions_Echo.TabIndex = 9;
+            // 
+            // Extensions
+            // 
+            this.Extensions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Extensions.Location = new System.Drawing.Point(3, 199);
+            this.Extensions.Multiline = true;
+            this.Extensions.Name = "Extensions";
+            this.Extensions.ReadOnly = true;
+            this.Extensions.Size = new System.Drawing.Size(212, 43);
+            this.Extensions.TabIndex = 8;
+            this.Extensions.Text = "Extensions";
+            // 
+            // Type_Echo
+            // 
+            this.Type_Echo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Type_Echo.Location = new System.Drawing.Point(221, 150);
+            this.Type_Echo.Multiline = true;
+            this.Type_Echo.Name = "Type_Echo";
+            this.Type_Echo.ReadOnly = true;
+            this.Type_Echo.Size = new System.Drawing.Size(212, 43);
+            this.Type_Echo.TabIndex = 7;
+            // 
+            // _Type
+            // 
+            this._Type.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._Type.Location = new System.Drawing.Point(3, 150);
+            this._Type.Multiline = true;
+            this._Type.Name = "_Type";
+            this._Type.ReadOnly = true;
+            this._Type.Size = new System.Drawing.Size(212, 43);
+            this._Type.TabIndex = 6;
+            this._Type.Text = "Type";
+            // 
+            // Res_Echo
+            // 
+            this.Res_Echo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Res_Echo.Location = new System.Drawing.Point(221, 101);
+            this.Res_Echo.Multiline = true;
+            this.Res_Echo.Name = "Res_Echo";
+            this.Res_Echo.ReadOnly = true;
+            this.Res_Echo.Size = new System.Drawing.Size(212, 43);
+            this.Res_Echo.TabIndex = 5;
+            // 
+            // _Res
+            // 
+            this._Res.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._Res.Location = new System.Drawing.Point(3, 101);
+            this._Res.Multiline = true;
+            this._Res.Name = "_Res";
+            this._Res.ReadOnly = true;
+            this._Res.Size = new System.Drawing.Size(212, 43);
+            this._Res.TabIndex = 4;
+            this._Res.Text = "Resolution";
+            // 
+            // Material_Echo
+            // 
+            this.Material_Echo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Material_Echo.Location = new System.Drawing.Point(221, 52);
+            this.Material_Echo.Multiline = true;
+            this.Material_Echo.Name = "Material_Echo";
+            this.Material_Echo.ReadOnly = true;
+            this.Material_Echo.Size = new System.Drawing.Size(212, 43);
+            this.Material_Echo.TabIndex = 3;
+            // 
+            // _Material
+            // 
+            this._Material.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._Material.Location = new System.Drawing.Point(3, 52);
+            this._Material.Multiline = true;
+            this._Material.Name = "_Material";
+            this._Material.ReadOnly = true;
+            this._Material.Size = new System.Drawing.Size(212, 43);
+            this._Material.TabIndex = 2;
+            this._Material.Text = "Material";
+            // 
+            // Catagory_Echo
+            // 
+            this.Catagory_Echo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Catagory_Echo.Location = new System.Drawing.Point(221, 3);
+            this.Catagory_Echo.Multiline = true;
+            this.Catagory_Echo.Name = "Catagory_Echo";
+            this.Catagory_Echo.ReadOnly = true;
+            this.Catagory_Echo.Size = new System.Drawing.Size(212, 43);
+            this.Catagory_Echo.TabIndex = 1;
+            // 
+            // _Catagory
+            // 
+            this._Catagory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._Catagory.Location = new System.Drawing.Point(3, 3);
+            this._Catagory.Multiline = true;
+            this._Catagory.Name = "_Catagory";
+            this._Catagory.ReadOnly = true;
+            this._Catagory.Size = new System.Drawing.Size(212, 43);
+            this._Catagory.TabIndex = 0;
+            this._Catagory.Text = "Catagory";
+            // 
+            // text_ActiveDirectory
+            // 
+            this.text_ActiveDirectory.Dock = System.Windows.Forms.DockStyle.Top;
+            this.text_ActiveDirectory.Location = new System.Drawing.Point(75, 0);
+            this.text_ActiveDirectory.Multiline = true;
+            this.text_ActiveDirectory.Name = "text_ActiveDirectory";
+            this.text_ActiveDirectory.ReadOnly = true;
+            this.text_ActiveDirectory.Size = new System.Drawing.Size(436, 28);
+            this.text_ActiveDirectory.TabIndex = 2;
+            // 
+            // button_ChooseActiveDirectory
+            // 
+            this.button_ChooseActiveDirectory.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button_ChooseActiveDirectory.Location = new System.Drawing.Point(0, 0);
+            this.button_ChooseActiveDirectory.Name = "button_ChooseActiveDirectory";
+            this.button_ChooseActiveDirectory.Size = new System.Drawing.Size(75, 273);
+            this.button_ChooseActiveDirectory.TabIndex = 1;
+            this.button_ChooseActiveDirectory.Text = "Pick Directory";
+            this.button_ChooseActiveDirectory.UseVisualStyleBackColor = true;
+            this.button_ChooseActiveDirectory.Click += new System.EventHandler(this.button_ChooseActiveDirectory_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(511, 45);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Output
+            // 
+            this.Output.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Output.Location = new System.Drawing.Point(0, 45);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.ReadOnly = true;
+            this.Output.Size = new System.Drawing.Size(511, 228);
+            this.Output.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Output);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 282);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(511, 273);
+            this.panel1.TabIndex = 6;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 558);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox Extensions_Echo;
+        private System.Windows.Forms.TextBox Extensions;
+        private System.Windows.Forms.TextBox Type_Echo;
+        private System.Windows.Forms.TextBox _Type;
+        private System.Windows.Forms.TextBox Res_Echo;
+        private System.Windows.Forms.TextBox _Res;
+        private System.Windows.Forms.TextBox Material_Echo;
+        private System.Windows.Forms.TextBox _Material;
+        private System.Windows.Forms.TextBox Catagory_Echo;
+        private System.Windows.Forms.TextBox _Catagory;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox text_ActiveDirectory;
+        private System.Windows.Forms.Button button_ChooseActiveDirectory;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BatchConverter/BatchConverter/MainWindow.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageMagick;
+using System.IO;
+using System.IO.Compression;
+
+namespace BatchConverter
+{
+
+    public partial class MainWindow : Form
+    {
+        private string ChoosenDirectory = "";
+        private string OutputDirectory {get { return ChoosenDirectory + @"\OUTPUT\";}}
+        private string PBR_Metalic { get { return ChoosenDirectory + @"\PBR_Metallic"; } }
+        private string Output_PBR_Metalic { get { return OutputDirectory + @"\PBR_Metallic.zip"; } }
+        private string PBR_Specular { get { return ChoosenDirectory + @"\PBR_Specular"; } }
+        private string Output_PBR_Specular { get { return OutputDirectory + @"\PBR_Specular.zip"; } }
+        private string Output_Web { get { return OutputDirectory + @"\Web\"; } }
+
+        public List<string> Catagory { get; set; }
+        public List<string> Material { get; set; }
+        public List<string> Resolution { get; set; }
+        public List<string>     TType       { get; set; }
+        public List<string> EExtensions { get; set; }
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_ChooseActiveDirectory_Click(object sender, EventArgs e)
+        {
+            //
+            // This event handler was created by double-clicking the window in the designer.
+            // It runs on the program's startup routine.
+            //
+            Output.AppendText("Opening Folder Browser\n");
+            using (FolderBrowserDialog br = new FolderBrowserDialog())
+            {
+                br.SelectedPath = @"E:\GameTextures\marble_brokenMonuments Example\marble_BrokenMonument Plain View - Copy";
+                DialogResult result = br.ShowDialog();
+                
+                if (result == DialogResult.OK)
+                {
+                    Catagory = new List<string>();
+                    Material = new List<string>();
+                    Resolution = new List<string>();
+                    TType = new List<string>();
+                    EExtensions = new List<string>();
+                    Catagory_Echo.Text = string.Empty;
+                    Res_Echo.Text = string.Empty;
+                    Material_Echo.Text = string.Empty;
+                    Type_Echo.Text = string.Empty;
+                    ChoosenDirectory = br.SelectedPath;
+                    Output.AppendText("Active Directory Set to:" + ChoosenDirectory+"\n");
+
+                    text_ActiveDirectory.Text = ChoosenDirectory;
+                    var Files = Directory.GetFiles(PBR_Metalic);
+                    Output.AppendText("Attempting to parse files in directory" + "\n");
+                    foreach (var file in Files)
+                    {
+                        try
+                        {
+                            string name = Path.GetFileName(file);
+                            if (!EExtensions.Contains(Path.GetExtension(file))) EExtensions.Add(Path.GetExtension(file));
+                            var bits = name.Split('_');
+                            if (bits.Length == 4)
+                            {
+                                
+                                if(!Catagory.Contains(bits[0])) Catagory.Add(bits[0]);
+                                if (!Material.Contains(bits[1])) Material.Add(bits[1]);
+                                if (!Resolution.Contains(bits[2])) Resolution.Add(bits[2]);
+                                if (!TType.Contains(bits[3].Substring(0, bits[3].IndexOf(Path.GetExtension(file))))) TType.Add(bits[3].Substring(0, bits[3].IndexOf(Path.GetExtension(file)))); 
+                            }
+                        }
+
+
+                        catch (Exception ee)
+                        {
+                            Output.AppendText("Failed Parsing files" + "\n");
+                        }
+                    }
+
+
+                    Files = Directory.GetFiles(PBR_Specular);
+
+                    foreach (var file in Files)
+                    {
+                        try
+                        {
+                            string name = Path.GetFileName(file);
+                            if (!EExtensions.Contains(Path.GetExtension(file))) EExtensions.Add(Path.GetExtension(file));
+                            var bits = name.Split('_');
+                            if (bits.Length == 4)
+                            {
+                                if (!Catagory.Contains(bits[0])) Catagory.Add(bits[0]);
+                                if (!Material.Contains(bits[1])) Material.Add(bits[1]);
+                                if (!Resolution.Contains(bits[2])) Resolution.Add(bits[2]);
+                                if (!TType.Contains(bits[3].Substring(0, bits[3].IndexOf(Path.GetExtension(file))))) TType.Add(bits[3].Substring(0, bits[3].IndexOf(Path.GetExtension(file))));
+                            }
+                        }
+
+
+                        catch (Exception ee)
+                        {
+                            Output.AppendText("Failed Parsing files" + "\n");
+
+                        }
+
+                    }
+                    Output.AppendText("Completed parsing files in directory" + "\n");
+
+
+                    Catagory_Echo.Text = string.Join(", ",Catagory);
+                    Res_Echo.Text = string.Join(", ", Resolution); ;
+                    Material_Echo.Text = string.Join(", ", Material); ;
+                    Type_Echo.Text = string.Join(", ", TType); ;
+                    Extensions_Echo.Text = string.Join(", ", EExtensions); ; 
+                    
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           Directory.CreateDirectory(OutputDirectory);
+           if (File.Exists(Output_PBR_Metalic))
+           {
+               Output.AppendText(Output_PBR_Metalic + " already exists, and is being overwritten! \n");
+               File.Delete(Output_PBR_Metalic);
+           }
+           Output.AppendText("Zip Started For: " + Output_PBR_Metalic + " \n");
+           ZipFile.CreateFromDirectory(PBR_Metalic, Output_PBR_Metalic);
+           Output.AppendText("zip finished! \n");
+
+           if (File.Exists(Output_PBR_Specular))
+           {
+               Output.AppendText(Output_PBR_Specular + " already exists, and is being overwritten! \n");
+               File.Delete(Output_PBR_Specular);
+           }
+           Output.AppendText("Zip Started For: " + Output_PBR_Specular + " \n");
+
+           ZipFile.CreateFromDirectory(PBR_Specular, Output_PBR_Specular);
+           Output.AppendText("zip finished! \n");
+
+           Directory.CreateDirectory(Output_Web);
+           Output.AppendText("Starting Downscaling for Web folder \n");
+
+           foreach (var file in Directory.GetFiles(PBR_Specular))
+           {
+               try
+               {
+                   if (file.EndsWith(".tga"))
+                   {
+                       var filename = Path.GetFileNameWithoutExtension(file);
+                       filename += ".jpg";
+                       MagickImage image = new MagickImage(file);
+                       image.Scale(512, 512);
+                       image.Write(Output_Web + filename);
+                       Output.AppendText("Completed: " + filename+" \n");
+
+                   }
+               }
+               catch (Exception ee)
+               {
+
+               }
+               
+           }
+
+        }
+
+       
+
+     
+    }
+}
